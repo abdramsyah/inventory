9ad88c582e141f58860240c284a36a70c4e22abc
abdramsyah <selaluilham5@gmail.com>
2022-07-24 12:05:22 +0700

    update

---
+++ application/config/site.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ *	Site Settings
+ *	
+ *	@author 	Noerman Agustiyan (@anoerman)
+ *	@version 	0.1.1
+ *	
+ *	Descriptions : 	You can add, edit or delete this site setting based on your 
+ *					preferences. Make sure the config name is unique.
+ *	
+ */
+
+$config['site_name']    = "dinas pemuda dan olahraga provinsi sumsel";
+$config['site_slogan']  = "Integrated Inventory System V3";
+$config['site_author']  = "MI";
+$config['site_company'] = "MI";
