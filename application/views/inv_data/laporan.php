b7b323c34ea0f637e921532bf8c6807ebbc4b441
abdramsyah <selaluilham5@gmail.com>
2022-08-02 09:55:22 +0700

    laporan

--- application/views/inv_data/laporan.php
+++ application/views/inv_data/laporan.php
@@ -5,7 +5,7 @@
 	<!-- Content Header (Page header) -->
 	<section class="content-header">
 	  <h1 align="center">
-	    Laporan Inventory Dinas Pe
+	    Laporan Inventory Dinas Kepemudaan dan Olahraga
 	    <small></small>
 	  </h1>
 	</section>
